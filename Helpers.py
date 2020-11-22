3026b6e99526ebad712a4825ab2435c51eaec925
Анна Бондаренко <mlokmlo1@gmail.com>
2020-11-22 18:56:15 +0200

    Development: Implement ability to retrieve number of pages for specified category.

--- Helpers.py
+++ Helpers.py
@@ -1,8 +1,8 @@
-import urllib
+from urllib import request
 from bs4 import BeautifulSoup
 
 def openURL(url):
-    response = urllib.request.urlopen(url)
+    response = request.urlopen(url)
     html = response.read()
     soup = BeautifulSoup(html, 'html.parser')
     return soup
--- main.py
+++ main.py
@@ -1,2 +1,12 @@
+import Constants as c
+import Helpers as h
+
+def getNumberOfPages(soup):
+    pagination = soup.find('ul', {'class': 'c_pagination_list'})
+    lists = pagination.find_all('li')
+    numberOfPages = int(lists[-2].text)
+    return numberOfPages
+
 if __name__ == '__main__':
-    print("Hello")
+    categorySoup = h.openURL(c.categoryURL)
+    print(getNumberOfPages(categorySoup))
