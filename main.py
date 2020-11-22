50b4f28810ab621040559b2d788509a20b958cae
Анна Бондаренко <mlokmlo1@gmail.com>
2020-11-22 19:08:59 +0200

    Development: Implement product links parsing for certain category.

--- main.py
+++ main.py
@@ -7,6 +7,21 @@ def getNumberOfPages(soup):
     numberOfPages = int(lists[-2].text)
     return numberOfPages
 
+def getProductLinks(soup):
+    products = soup.find_all('div', {'class': 'prdct_i bx_catalog_item_container aaa typeCatalog'})
+    return list(map(lambda product: c.baseURL + product.find('div', {'class': 'name-wrap'}).a.get('href'), products))
+
 if __name__ == '__main__':
     categorySoup = h.openURL(c.categoryURL)
-    print(getNumberOfPages(categorySoup))
+    numberOfPages = getNumberOfPages(categorySoup)
+
+    print(f'Found {numberOfPages} pages in category: {c.categoryURL}')
+
+    products = []
+
+    for pageNumber in range(1, numberOfPages):
+        pageLink = c.categoryURL + f'?PAGEN_1={pageNumber}'
+        print(f'Parsing page: {pageLink}')
+        pageSoup = h.openURL(pageLink)
+        productLinks = getProductLinks(pageSoup)
+        print(productLinks)
