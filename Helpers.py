b14a6b096ddd09417bf1e00f8c08f1643bb4b793
Анна Бондаренко <mlokmlo1@gmail.com>
2020-11-25 21:31:05 +0200

    General: Add docs for helpers methods.

--- Helpers.py
+++ Helpers.py
@@ -1,6 +1,8 @@
 from urllib import request
 from bs4 import BeautifulSoup
 
+# Performs simple GET request to endpoint.
+# Constructs object using bs4 lib.
 def openURL(url):
     response = request.urlopen(url)
     html = response.read()
